80f3dc4910e7c1a14ee8f3a18322e73580507bdd
Nacho-Keiniger <37520232+Nacho-Keiniger@users.noreply.github.com>
2020-10-13 21:14:55 -0300

    agrega productos en ordenes de compra

--- scripts.js
+++ scripts.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){
-    $('[data-toggle="popover"]').popover();   
-  });
-    
-function agregarFila(){
-    var botonAgregar= document.getElementById('botonAgregar');
-    var codigo      = document.getElementById('productoCodigo').value;
+$(document).ready(function () {
+    $('[data-toggle="popover"]').popover();
+});
+
+function agregarFila() {
+    var botonAgregar = document.getElementById('botonAgregar');
+    var codigo = document.getElementById('productoCodigo').value;
     var descripcion = document.getElementById('productoDescripcion').value;
-    var proveedor   = document.getElementById('productoProveedor').value;
-    var table       = document.getElementsByTagName('table')[0];
-    var newRow      = table.insertRow(table.rows.length);
+    var proveedor = document.getElementById('productoProveedor').value;
+    var table = document.getElementsByTagName('table')[0];
+    var newRow = table.insertRow(table.rows.length);
 
     var cel1 = newRow.insertCell(0);
     var cel2 = newRow.insertCell(1);
@@ -21,3 +21,26 @@ function agregarFila(){
     cel4.innerHTML = proveedor;
 }
 
+function agregarProducto() {
+    document.getElementById('nuevoProducto').insertAdjacentHTML('beforebegin', `
+    <div style="display: flex; align-items: center; flex-wrap: wrap;">
+        <div class="col" style="flex-grow: 10;">
+            <div class="form-group">
+                <select class="form-control" id="sel1" placeholder="Categoría" width="50%">
+                    <option>Producto</option>
+                    <option>Paquete de galletitas de 150gr</option>
+                    <option>Paquete de galletitas de 400gr</option>
+                    <option>Paquete de galletitas de 700gr</option>
+                </select>
+            </div>
+        </div>
+    <div style="display: inline-block; width: 15%;">
+        <input type="number" class="form-control" id="cantidad" placeholder="Cantidad">
+    </div>
+        <div class="col text-center">
+            <a onclick="agregarProducto()" type="submit" data-toggle="popover" data-trigger="hover" title="Logica" data-content="Aca iria la lógica" style="margin-top: 8px;">
+                <i class="fas fa-plus-circle h1"></i>
+            </a>
+        </div>
+    </div>`);
+}
